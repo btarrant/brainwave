f5ab9cbf1d1ff1d62861378829c6b5410683697e
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-11 13:37:35 -0400

    Build the header component and make responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,41 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import { useLocation } from "react-router-dom";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
 
   return (
+    // -------------------------------------
+    // Header Component
+    // -------------------------------------
     <div
-      className="fixed top-0 z-50 bg-n-8/90
-    backdrop-blur-sm border-b border-n-6 
-    lg:bg-n-8/90 lg:backdrop-blur-sm"
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 
+    lg:bg-n-8/90 lg:backdrop-blur-sm
+    ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}
     >
       <div
         className="flex items-center px-5 lg:px-7.5
@@ -18,9 +44,13 @@ const Header = () => {
         <a className="block w-[12rem] xl:mr-8 href=#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
+        {/* ------------------------------------- */}
+        {/* Navigation Bar */}
+        {/* ------------------------------------- */}
         <nav
-          className="hidden fixed top-[5rem] left-0 right-0 bottom-0
-        bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
+          className={`${openNavigation ? "flex" : "hidden"}
+          fixed top-[5rem] left-0 right-0 bottom-0
+        bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div
             className="relative z-2 flex flex-col items-center
@@ -30,17 +60,36 @@ const Header = () => {
               <a
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl
                 uppercase text-n-1 transition-colors hover:text-color-1
                 ${item.onlyMobile ? "lg:hidden" : ""}
                 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold
-                ${item.url === pathname.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"}`}
+                ${item.url === pathname.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"}
+                lg:leading-5 lg:hover:text-n-1 xl:px-x-12`}
               >
                 {item.title}
               </a>
             ))}
           </div>
+          {/* ------------------------------------- */}
+          {/* Hamburger Menu */}
+          {/* ------------------------------------- */}
+          <HamburgerMenu />
         </nav>
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50
+          transition-colors hover:text-n-1 lg:block"
+        >
+          New Account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign In
+        </Button>
+        <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleNavigation}>
+          <MenuSvg openNavigation={openNavigation} />
+        </Button>
       </div>
     </div>
   );
