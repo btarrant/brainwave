72312a81b876d316cfa797a97ed57d24c9904dbe
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-11 00:05:36 -0400

    Fix inline-flex type, line 4

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative infline-flex
+  const classes = `button relative inline-flex
   items-center justify-center h-11 transition-colors
   hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"}
   ${className || ""}`;
