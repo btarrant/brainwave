b434a9a6be98ff94a0ce61d85dee4638a62b842e
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-12 00:04:55 -0400

    Add elements and parallax motion to hero

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,11 +5,8 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
     <div
       id={id}
       className={`relative ${
-        customPaddings ||
-        `p-10 lg:py-16 xl:py-20
-        ${crosses ? "lg:py-32 xl:py-40" : ""}
-        ${className || ""}`
-      }`}
+        customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } ${className || ""}`}
     >
       {children}
       <div
