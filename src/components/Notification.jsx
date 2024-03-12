344f5f1a35b8158c45045e3512a1048c93630b61
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-12 14:59:14 -0400

    Refactor

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { notificationImages } from "../constants";
+import { notification1 } from "../assets";
 
 const Notification = ({ className, title }) => {
   return (
@@ -7,6 +8,7 @@ const Notification = ({ className, title }) => {
       className={`${className || ""} flex items-center p-4 pr-6 bg-n-9/40 
       backdrop-blur border border-n1/10 rounded-2xl gap-5`}
     >
+      <img src={notification1} width={62} height={62} alt="image" />
       <div className="flex-1">
         <h6 className="mb-1 font-semibold text-base">{title}</h6>
         <div className="flex items-center justify-between">
