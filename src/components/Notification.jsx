728fe03a871f202fecd9fee0757712bc049e64a1
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-12 15:17:58 -0400

    Add logos to bottom of hero

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -22,6 +22,7 @@ const Notification = ({ className, title }) => {
               </li>
             ))}
           </ul>
+          <div className="body-2 text-n-13">1m ago</div>
         </div>
       </div>
     </div>
