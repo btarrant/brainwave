bc8f28b25cf3a098e68e05a418ac998d5b345916
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-13 17:16:16 -0400

    Add 3rd service box to layout

--- src/components/design/Services.jsx
+++ src/components/design/Services.jsx
@@ -15,7 +15,7 @@ export const Gradient = () => {
   );
 };
 
-export const PhotChatMessage = () => {
+export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
       Hey Brainwave, enhance this photo
@@ -29,12 +29,7 @@ export const VideoChatMessage = () => {
     <div className="absolute top-8 left-[3.125rem] w-full max-w-[14rem] pt-2.5 pr-2.5 pb-7 pl-5 bg-n-6 rounded-t-xl rounded-br-xl font-code text-base md:max-w-[17.5rem]">
       Video generated!
       <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1 rounded-[0.75rem]">
-        <img
-          src={brainwaveWhiteSymbol}
-          width={26}
-          height={26}
-          alt="Brainwave"
-        />
+        <img src={brainwaveWhiteSymbol} width={26} height={26} alt="Brainwave" />
       </div>
       <p className="tagline absolute right-2.5 bottom-1 text-[0.625rem] text-n-3 uppercase">
         just now
@@ -50,13 +45,7 @@ export const VideoChatMessage = () => {
 export const VideoBar = () => {
   return (
     <div className="absolute left-0 bottom-0 w-full flex items-center p-6">
-      <img
-        src={play}
-        width={24}
-        height={24}
-        alt="Play"
-        className="object-contain mr-3"
-      />
+      <img src={play} width={24} height={24} alt="Play" className="object-contain mr-3" />
 
       <div className="flex-1 bg-[#D9D9D9]">
         <div className="w-1/2 h-0.5 bg-color-1"></div>
