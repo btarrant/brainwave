fd0ac999bfd1af5219d17512ef7f15a8d3fcd6d1
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-16 20:25:38 -0400

    Add images and styles to Roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,8 @@
-import { grid } from "../assets";
+import { check2, grid, loading1 } from "../assets";
 import { roadmap } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
+import Tagline from "./Tagline";
 
 const Roadmap = () => {
   return (
@@ -13,10 +14,44 @@ const Roadmap = () => {
             const status = item.status === "done" ? "Done" : "In progress";
 
             return (
-              <div key={item.id}>
+              <div
+                key={item.id}
+                className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
+                  item.colorful ? "bg-conic-gradient" : "bg-n-6"
+                }`}
+              >
                 <div>
                   <div>
-                    <img src={grid} />
+                    <img src={grid} className="w-full" width={550} height={550} alt="Grid" />
+                  </div>
+                  <div className="relative z-1">
+                    <div className="flex items-center justify-between">
+                      <Tagline>{item.date}</Tagline>
+                      {/* -------------------------- */}
+                      {/* Status */}
+                      {/* -------------------------- */}
+                      <div>
+                        <img
+                          src={item.status === "done" ? check2 : loading1}
+                          className="mr-2.5"
+                          width={16}
+                          height={16}
+                          alt={status}
+                        />
+                        <div className="tagline">{status}</div>
+                      </div>
+                    </div>
+                    <div className="mb-10">
+                      <img
+                        src={item.imageUrl}
+                        className="w-full"
+                        width={630}
+                        height={420}
+                        alt={item.title}
+                      />
+                    </div>
+                    <h4 className="h4 mb-4">{item.title}</h4>
+                    <p className="body-2 text-n-4">{item.text}</p>
                   </div>
                 </div>
               </div>
